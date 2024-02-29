c5bf4cb948c77003b8bca708610e5c04eea7fbbb
ogdmerlin <ogdmerlin@gmail.com>
2024-02-29 19:45:34 +0000

    Restructured my terraform code

--- Socks_Shop/Terraform/main.tf
+++ Socks_Shop/Terraform/main.tf
@@ -1,68 +1,117 @@
-#Main configuration file for Terraform
+# Copyright (c) HashiCorp, Inc.
 
-# Creating local variables to eradicate multiple usage of the same string.
+provider "aws" {
+  region = var.region
+}
+
+# Filter out local zones, which are not currently supported 
+# with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
 locals {
-  setup_name = "socks-shop"
+  cluster_name = "socksShop-eks-${random_string.suffix.result}"
 }
 
-# Creating a VPC
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.1.0.0/16"
+resource "random_string" "suffix" {
+  length  = 5
+  special = false
+}
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "socks-shop-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.27"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+}
+
+
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "4.7.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
+resource "aws_eks_addon" "ebs-csi" {
+  cluster_name             = module.eks.cluster_name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.20.0-eksbuild.1"
+  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
   tags = {
-    Name = "${local.setup_name}-vpc" //socks-shop-vpc
+    "eks_addon" = "ebs-csi"
+    "terraform" = "true"
   }
 }
-
-# Creating a subnet
-resource "aws_subnet" "subnet" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.1.0.0/16"
-
-  tags = {
-    Name = "${local.setup_name}-subnet" //socks-shop-subnet
-  }
-}
-
-#Creating the Ec2 instance for our web application
-resource "aws_instance" "web" {
-  ami           = "ami-0c7217cdde317cfec"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.subnet.id
-
-  tags = {
-    Name = "${local.setup_name}-instance" //socks-shop-instance
-  }
-}
-
-# Main Terraform configuration for AWS EKS cluster
-resource "aws_eks_cluster" "example" {
-  name     = "example"
-  role_arn = aws_iam_role.example.arn
-  vpc_config {
-    subnet_ids = [aws_subnet.subnet.id]
-  }
-}
-
-resource "aws_iam_role" "example" {
-  name = "example"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "eks.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.example.name
-}
-
-
---
+++ Socks_Shop/Terraform/variables.tf
@@ -0,0 +1,7 @@
+# Copyright (c) HashiCorp, Inc.
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
